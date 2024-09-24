9499939fbba5829addc1232611e729c54000aa30
Jorge Antony Zarate Davila <antonijorge987@gmail.com>
2024-09-23 23:59:05 -0500

    update

--- script.js
+++ script.js
@@ -1,6 +1,56 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+//Get Elements from DOM
+const btns = document.querySelectorAll('.buttons button');
+const imgs = document.querySelectorAll('.images img');
+
+//Add a click event to all buttons
+for(let i = 1; i < btns.length; i++) {
+  btns[i].addEventListener('click', filterImg); // Closing bracket was missing
 }
+
+//Set active button on click
+function setActiveBtn(e) {
+  //Remove active class from all buttons
+  btns.forEach(btn => {
+    btn.classList.remove('btn-clicked');
+  });
+
+  //Add active class to clicked button
+  e.target.classList.add('btn-clicked');
+}
+
+//Filter Images
+function filterImg(e) {
+  //Run the active button function
+  setActiveBtn(e);
+
+  //Loop through all images
+  imgs.forEach(img => {
+    //Expand all images
+    img.classList.remove('img-shrink');
+    img.classList.add('img-expand');
+
+    //Get data from data attributes
+    const imgType = parseInt(img.dataset.img);
+    const btnType = parseInt(e.target.dataset.btn);
+
+    // If the image type and the type of the clicked button are NOT the same
+    if (imgType !== btnType) {
+      //Hide the image
+      img.classList.remove('img-expand');
+      img.classList.add('img-shrink');
+    }
+  });
+}
+
+//Set click event for the 'ALL' button
+btns[0].addEventListener('click', (e) => {
+  //Run the active button function
+  setActiveBtn(e);
+
+  //Loop through all images
+  imgs.forEach(img => {
+    //Expand all images
+    img.classList.remove('img-shrink');
+    img.classList.add('img-expand');
+  });
+});
